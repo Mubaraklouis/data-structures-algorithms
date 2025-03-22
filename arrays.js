34fd26193eab3586f83659c5282056d0f3f16173
mubarak <luismubarak@gmail.com>
2025-03-22 23:27:23 +0200

    implement my own javascript insert function

--- arrays.js
+++ arrays.js
@@ -1,20 +1,35 @@
 const arr = [1, 2, 3, 4, 5];
 
-let lastIndex = arr.length - 1;
+// arr[4]=arr[3];
+// arr[3]=arr[2];
+// arr[2]=arr[1];
+// arr[1]=arr[0];
+// arr[0]=10
 
-let newIndex = lastIndex + 1;
 
-arr[newIndex]=arr[4];
-arr[4]=arr[3];
-arr[3]=arr[2];
-arr[2]=arr[1];
-arr[1]=arr[0];
-arr[0]=10
-
-const arrLength = arr.length;
-
+function insertFirst(arr,num){
+    const arrLength = arr.length;
+    let lastIndex = arrLength - 1;
+    let newIndex = lastIndex + 1;
+    arr[newIndex]=arr[lastIndex];
 
 for (let i = arrLength-1; i >=0; i--) {
-    console.log(arr[i]);    
+    if (i === 0) {
+      arr[i] = num;
+    }
+    else {
+      arr[i] = arr[i-1];
+    }
+ }
+
 }
 
+
+function verifyInsertFirst(){
+   let arr = [1, 2, 3, 4, 5];
+    insertFirst(arr,30);
+    console.log(arr);
+}
+
+
+verifyInsertFirst(); // [10, 1, 2, 3, 4]
